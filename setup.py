e1e393c7d3bd5ddaf910b031a9cb7cd618d66aca
Nathan West <Lucretiel@gmail.com>
2017-05-05 19:10:12 -0400

    version bump; added test

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def getfile(filename):
 
 setup(
     name='makepass',
-    version='0.9.3',
+    version='0.9.4',
     packages=[
         'makepass'
     ],
--- util/test.sh
+++ util/test.sh
@@ -8,11 +8,25 @@ then
     exit 1
 fi
 
-if test "$(cat "$OUTPUT_FILE" | tr -d ' \n\t' | wc -c)" -ge 24
-then
-	echo "Successfully generated password!"
-else
-	echo "make_pass exited successfully, but the password isn't large enough"
-	echo "Password: '$(cat "$OUTPUT_FILE")'"
-	exit 1
-fi
+test_or () {
+	error_msg="$1"
+	shift
+
+	if ! "$@"
+	then
+		echo "$error_msg"
+		echo "Password: '$(cat "$OUTPUT_FILE")'"
+		exit 1
+	fi
+}
+
+
+test_or "make_pass exited successfully, but the password isn't large enough" \
+	test "$(cat "$OUTPUT_FILE" | tr -d ' \n\t' | wc -c)" -ge 24
+
+
+PATTERN='^([A-Z][a-z]{3,7}){4}[0-9]$'
+test_or "make_pass exited successfully, but the password doesn't match the pattern $PATTERN" \
+	grep -Eq "$PATTERN" "$OUTPUT_FILE"
+
+echo "All tests passed!"
